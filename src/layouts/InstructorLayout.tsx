fdea9b20fb173885eddb14e8bd9a9cbecd7b8b14
Quyen-Peter <quyentrinh040@gmail.com>
2026-06-11 10:30:27 +0700

    fix folder

--- src/layouts/InstructorLayout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-type InstructorLayoutProps = {
-  children: ReactNode;
-};
-
-export function InstructorLayout({ children }: InstructorLayoutProps) {
-  return <>{children}</>;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./styles/global.css";
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
